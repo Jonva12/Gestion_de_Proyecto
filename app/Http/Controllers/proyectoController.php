f9db20d117921f5c5f9ebdfee9191197fee7059b
Jonva12 <jonva12@hotmail.com>
2019-11-15 11:00:03 +0100

    ejercicio 7 inicio

--- app/Http/Controllers/proyectoController.php
+++ app/Http/Controllers/proyectoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Proyectos;
 
 class proyectoController extends Controller
 {
@@ -13,7 +14,8 @@ class proyectoController extends Controller
      */
     public function index()
     {
-        //
+        $proyectos = Proyectos::all();
+        return view('proyectos/index', ['proyectos'=>$proyectos]);
     }
 
     /**
